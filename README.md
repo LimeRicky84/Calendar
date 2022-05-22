6434b9a3703d0ab042163802961f5c4a5e24e813
Ricma <ric.madse@gmail.com>
2022-05-21 20:22:47 -0600

    added js functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calendar
+
+This is a challenge to create a single day planning calendar that color codes the time blocks for future current and past. it will add saved tasks and the tasks will persist if the page reloads.  I'm adding a "clear all" button for fun.
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,66 @@
 // add current time in header
 var timeDisplayEl = $("#rightNow")
+var savedTask = document.querySelector("#timeBlock")
+var taskList = document.querySelector("#timeBlock")
+var saveButton = document.querySelector("#save-button")
+var clearButton = document.querySelector("#clear-button")
 
+var tasks = [];
 
 function timeDate() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
-  }
+}
 
-  setInterval(timeDate, 1000);
+
+// renders items on page as <p> elements
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    for (var i = 0; i < tasks.length; i++) {
+        var task = tasks[i];
+
+        var p = document.createElement('p')
+        p.textContent = task;
+        p.setAttribute("data-index", i);
+
+        var button = document.createElement("button");
+        button.textContent = "Finished"
+
+        p.appendChild(button)
+        taskList.appendChild(p)
+    }
+}
+
+function storeTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault()
+
+    var taskText = savedTask.Value.trim()
+    if (taskText === "") {
+        return;
+    }
+
+    tasks.push(taskText)
+    savedTask.value = ""
+
+    storeTasks()
+    renderTasks()
+});
 
 //   function to keep inputs saved after page reload
+function init() {
+    var storedTasks = JSON.parse(localStorage.getItem("tasks"))
 
+    if (storedTasks !== null) {
+        tasks = storedTasks
+    }
+    renderTasks()
+}
 
-
-
+setInterval(timeDate, 1000);
+init()
 //   function to color code the past, present and future
